4df3bf57fb11ba780688e9b08b4a208e04e60cf8
Dani Grajales <dani22.24gra@gmail.com>
2022-04-19 12:09:32 -0500

    Catualizacion apartado productos

--- Majestic/js/app.js
+++ Majestic/js/app.js
@@ -1,3 +1,8 @@
 document.querySelector(".menu").addEventListener("click", () => {
   document.querySelector(".navbarFloat").classList.toggle("show");
 });
+
+document.querySelector(".btn").addEventListener("click", () => {
+  document.querySelector(".apartado1").classList.toggle("visible");
+  document.querySelector(".cards").classList.toggle("invisible");
+});
